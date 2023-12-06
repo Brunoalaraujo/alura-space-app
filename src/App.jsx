6162e0ca70aadd1cf5984602734b80ff8b21a93f
Bruno Araujo <brunoandradel.a.dev@gmail.com>
2023-12-05 22:33:00 -0300

    :sparkles: feat: Header e Aside

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components"
 import GlobalStyles from "./components/GlobalStyles"
+import Cabecalho from "./components/Header"
+import BarraLateral from "./components/BarraLateral"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -14,6 +16,8 @@ function App() {
   return (
     <FundoGradiente>
      <GlobalStyles/>
+     <Cabecalho/>
+     <BarraLateral/>
     </FundoGradiente>
   )
 }
