04c56a6a96feec01195661b93624faa2e8e1849d
Bruno Araujo <brunoandradel.a.dev@gmail.com>
2023-12-09 23:39:12 -0300

    :sparkles: feat Figure

--- src/components/Galeria/Imagem/index.jsx
+++ src/components/Galeria/Imagem/index.jsx
@@ -1,13 +1,21 @@
-const Imagem = ({foto}) => {
+import styled from "styled-components"
+import fotos from "./fotos.json"
+
+const Figure = styled.figure`   
+width: ${(props) => (props.$expandida ? '90%' : '460px')};
+
+`
+
+const Imagem = () => {
     return (
-        <figure>
+        <Figure key={fotos.id} src={fotos.path}>
             <h3>titulo</h3>
             <footer>
                 <p>fonte</p>
                 <button>favorito</button>
                 <button>Expandir</button>
             </footer>
-        </figure>
+        </Figure>
     )
 }
 
